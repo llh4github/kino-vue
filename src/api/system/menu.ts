bffe57381e1841322e97ab327d9e1ec8edc5b5ac
llh4github <lilinhong_coding@foxmail.com>
2023-05-29 21:06:59 +0800

    wip: 菜单页面基本布局

---
+++ src/api/system/menu.ts
@@ -0,0 +1,94 @@
+import { getRequest, postRequest, putRequest } from "@/utils/service";
+
+export function tree(id: number) {
+  return getRequest<MenuTreeData>("/inner/menu/tree", id)
+}
+
+export function treeList(parentId?: number) {
+  return getRequest<MenuTreeData[]>("/inner/menu/treeList", parentId)
+}
+
+export function updateData(data: MenuUpdateData) {
+  return putRequest<boolean>("/inner/menu", data)
+}
+
+export function addData(data: MenuAddData) {
+  return postRequest<boolean>("/inner/menu", data)
+}
+
+
+// --- 下面是类型定义 ---
+
+
+/**
+ * MenuTreeData : 菜单树数据
+ */
+export interface MenuTreeData {
+  children: MenuTreeData [];
+  /**
+   * 创建者ID
+   */
+  createdBy?: number;
+  /**
+   * 创建时间
+   */
+  createdTime: Date;
+  /**
+   * 数据ID
+   */
+  id: number;
+  /**
+   * 名称
+   */
+  name: string;
+  /**
+   * 上级菜单ID
+   */
+  parentId?: number | null;
+  /**
+   * 路由路径
+   */
+  router: string;
+  /**
+   * 更新者ID
+   */
+  updatedBy?: number;
+  /**
+   * 更新时间
+   */
+  updatedTime: Date;
+}
+
+export interface MenuUpdateData {
+  /*
+  * 数据ID
+  */
+  id: number;
+  /**
+   * 菜单名
+   */
+  name: string;
+  /**
+   * 上级菜单id
+   */
+  parentId?: number;
+  /**
+   * 前端路径
+   */
+  router: string;
+}
+
+export interface MenuAddData{
+  /**
+   * 菜单名
+   */
+  name: string;
+  /**
+   * 上级菜单id
+   */
+  parentId?: number;
+  /**
+   * 前端路径
+   */
+  router: string;
+}
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,11 +1,100 @@
 <script setup lang="ts">
+import { MenuTreeData, treeList } from "@/api/system/menu";
+import { onMounted, reactive, ref } from "vue";
+import { ElTreeV2, FormInstance } from 'element-plus'
+import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
+import { Refresh, Search } from "@element-plus/icons-vue";
 
+const loading = ref(false)
+defineOptions({
+  name: "Menu-Index-Page",
+})
+const tree = ref<MenuTreeData[]>([])
+const fetchTree = async () => {
+  tree.value = (await treeList()).data
+}
+
+const searchData = reactive({
+  router: undefined,
+  name: undefined,
+})
+
+const searchFormRef = ref<FormInstance | null>(null)
+const treeRef = ref<InstanceType<typeof ElTreeV2>>()
+const handleSearch = () => {
+  treeRef.value!.filter(searchData.name)
+}
+const filterMethod = (query: any, node: TreeNode) => {
+  console.log(query,node)
+  return node.label!.includes(query)
+}
+const resetSearch = () => {
+  if (searchFormRef.value) {
+    searchFormRef.value?.resetFields();
+  } else {
+    console.debug(`搜索表单的引用为空！ ${searchFormRef.value}`)
+  }
+}
+const treeProps = {
+  label: 'name',
+  children: "children",
+}
+onMounted(() => {
+  fetchTree()
+})
 </script>
-
 <template>
-菜单页面
+  <div class="app-container">
+    <el-card v-loading="loading" shadow="never" class="search-wrapper">
+      <el-form ref="searchFormRef" :inline="true" :model="searchData">
+        <el-form-item prop="name" label="菜单名">
+          <el-input v-model="searchData.name" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="code" label="路由路径">
+          <el-input v-model="searchData.router" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" :icon="Search" @click="handleSearch">查询</el-button>
+          <el-button :icon="Refresh" @click="resetSearch">重置</el-button>
+        </el-form-item>
+      </el-form>
+
+    </el-card>
+
+    <el-card v-loading="loading" shadow="never">
+      <el-tree-v2
+        ref="treeRef"
+        :data="tree"
+        :filter-method="handleSearch"
+        :props="treeProps"
+        show-checkbox />
+    </el-card>
+  </div>
 </template>
 
 <style scoped lang="scss">
 
+
+.search-wrapper {
+  margin-bottom: 20px;
+
+  :deep(.el-card__body) {
+    padding-bottom: 2px;
+  }
+}
+
+.toolbar-wrapper {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.table-wrapper {
+  margin-bottom: 20px;
+}
+
+.pager-wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
